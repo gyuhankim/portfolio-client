4576d155baacfd3d650ecb6361fe7203fff5df21
Joseph Kim <gyuhankim@gmail.com>
2018-10-11 13:43:52 -0400

    test

--- src/components/cubes.js
+++ src/components/cubes.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Unity, { UnityContent } from "react-unity-webgl";
 
 import '../styles/cubes.css';
+
+import cubesHTML from '../index.html';
 
 export class Cubes extends React.Component {
   constructor(props) {
     super(props);
-
-    this.unityContent = new UnityContent (
-      "Build/WebGL_Build.json",
-      "Build/UnityLoader.js"
-    );
 
   }
 
   render() {
 
     return (
-      <Unity unityContent={this.unityContent} height="720px" width="1280px" />
+      <div>
+        <cubesHTML />
+      </div>
     );
   }
     
